0a0ba19193fe53f80da3983d155f8d1cfbfc6458
DESKTOP-PR2BK7P\csi23-mserina <csi23-mserina@colegioaltair.net>
2023-12-18 14:43:43 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace ejercicio2Arraymsm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fila = 10;
+            int conlumna = 10;
+            int celdas = 101;
+
+            int[,] matriz = new int [fila, conlumna];
+
+            //filas
+            for ( int i= 0; i < fila; i++)
+            {
+                for (int j = 0; j < conlumna; j++)
+                {
+                    matriz[i, j] = celdas;
+                    celdas--;
+                    Console.Write(matriz[i,j] + " ");
+                }
+                Console.WriteLine("|");
+            }
+
+
+            for (int i= 0;i < 101; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
